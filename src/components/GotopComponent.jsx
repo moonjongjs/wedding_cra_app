84e8b0df66c72dcaee84dcffab46e111c9213757
moonjongjs <moonseonjong@naver.com>
2023-04-19 22:00:15 +0900

    Updates

--- src/components/GotopComponent.jsx
+++
@@ -1,7 +0,0 @@
-export default  function GotopComponent(){
-    return(
-        <span className="go-top fade-in">
-            <a href="#wrap"><i className="fa fa-angle-up"></i></a>
-        </span>
-    )
-}
